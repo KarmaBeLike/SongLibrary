8138e2ee99c8d9490c9a2a4efe1beb2824b55917
Kamila <113854806+KarmaBeLike@users.noreply.github.com>
2024-09-28 00:02:14 +0500

    db conn

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
 
+	"github.com/KarmaBeLike/SongLibrary/config"
+	"github.com/KarmaBeLike/SongLibrary/internal/database"
 	"github.com/KarmaBeLike/SongLibrary/internal/routers"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		slog.Error("config loh", slog.Any("error", err))
+		return
+	}
+
+	db, err := database.OpenDB(cfg)
+	if err != nil {
+		slog.Error("bd loh", slog.Any("error", err))
+		return
+	}
+	defer db.Close()
+	slog.Info("health check", slog.Any("db ping", db.Ping()))
+
 	router := routers.SetupRoutes()
 
 	log.Println("Server is runnig on port 8080...")
-	err := http.ListenAndServe(":8080", router)
+	err = http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal("Error starting server", err)
 	}
---
+++ internal/database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/KarmaBeLike/SongLibrary/config"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+)
+
+func OpenDB(cfg *config.Config) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, errors.Wrap(err, "open sql")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "connection is not established")
+	}
+
+	return db, nil
+}
